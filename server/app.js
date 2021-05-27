b73b10dd857737e2bcf49f18193623f7454df1f5
Shane Sutro <shanesutro@gmail.com>
2021-05-27 10:33:15 -0600

    Add express app

---
+++ server/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+module.exports = app;
