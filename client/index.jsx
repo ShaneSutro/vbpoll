e586c89858a73e14ac5474007b25ebb458f8b2eb
Shane Sutro <shanesutro@gmail.com>
2021-05-27 11:46:15 -0600

    Updating packages

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app.jsx';
+import App from './components/app';
+// eslint-disable-next-line no-unused-vars
 import css from '../public/style.css';
 
 // eslint-disable-next-line no-undef
