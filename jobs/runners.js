bdbd596945c151a656157ad74a16bde71a483d16
Shane Sutro <shanesutro@gmail.com>
2021-06-02 17:47:07 -0600

    Fix error handling

--- jobs/runners.js
+++ jobs/runners.js
@@ -5,10 +5,30 @@ const moment = require('moment');
 
 const vesta = new Vesta('', '');
 
-const convertLine = (string) => vesta.characterArrayFromString(string).slice(0, 1)[0];
+const convertLine = (string) =>
+  vesta.characterArrayFromString(string).slice(0, 1)[0];
 
 const convertAnswerLine = (option, answer, pct, isWinning) => {
-  const rainbowLine = ['redBlock', 'orangeBlock', 'yellowBlock', 'greenBlock', 'blueBlock', 'violetBlock', 'redBlock', 'orangeBlock', 'yellowBlock', 'greenBlock', 'blueBlock', 'violetBlock', 'redBlock', 'orangeBlock', 'yellowBlock', 'greenBlock', 'blueBlock', 'violetBlock'];
+  const rainbowLine = [
+    'redBlock',
+    'orangeBlock',
+    'yellowBlock',
+    'greenBlock',
+    'blueBlock',
+    'violetBlock',
+    'redBlock',
+    'orangeBlock',
+    'yellowBlock',
+    'greenBlock',
+    'blueBlock',
+    'violetBlock',
+    'redBlock',
+    'orangeBlock',
+    'yellowBlock',
+    'greenBlock',
+    'blueBlock',
+    'violetBlock',
+  ];
   let line = `${option}) ${answer}`;
   const spacer = 22 - (pct.length + 1) - line.length;
   for (let i = 0; i <= spacer; i += 1) {
@@ -63,57 +83,96 @@ const calculatePercentages = (votes) => {
 
 const sendMessage = (creds, characters) => {
   console.log(`Sending to ${creds.subId}`);
-  fetch(`https://platform.vestaboard.com/subscriptions/${creds.subId}/message`, {
-    method: 'POST',
-    headers: {
-      'X-Vestaboard-Api-Key': creds.apiKey,
-      'X-Vestaboard-Api-Secret': creds.apiSecret,
+  fetch(
+    `https://platform.vestaboard.com/subscriptions/${creds.subId}/message`,
+    {
+      method: 'POST',
+      headers: {
+        'X-Vestaboard-Api-Key': creds.apiKey,
+        'X-Vestaboard-Api-Secret': creds.apiSecret,
+      },
+      body: JSON.stringify({ characters }),
     },
-    body: JSON.stringify({ characters }),
-  })
-    .then((response) => console.log(response.status));
+  ).then((response) => console.log(response.status));
 };
 
 module.exports = {
   'c5d472aa-260d-4e7e-84ab-f069a3c83729': async (data) => {
-    try: {
-
-        const pollInfo = await poll.getBySub(data.subId);
-        let pollClosed = false;
-        if (!pollInfo.poll.isOpen || new Date() > moment(pollInfo.poll.openUntil).toDate()) {
-          console.log('Poll is closed or expired');
-          pollClosed = true;
-        }
-        const messageArray = [
-          // Title line
-          [
-            65, 65, 65, 65, 65, 65, 65, 0, 0, 16, 15, 12, 12, 0, 0, 65, 65, 65, 65, 65, 65, 65,
-          ],
-        ];
-        messageArray.push(convertLine(`  vbpoll.app/${pollInfo.pollID}  `));
-        if (pollInfo.voteCounts.totalVotes === 1 && !pollClosed) {
-          messageArray.push(convertLine('        1 vote        '));
-        } else if (pollClosed) {
-          console.log('Poll is closed');
-          messageArray.push(convertLine(`  closed - ${pollInfo.voteCounts.totalVotes} vote${pollInfo.voteCounts.totalVotes > 1 ? 's' : ''}  `));
-        } else {
-          messageArray.push(convertLine(`       ${pollInfo.voteCounts.totalVotes} votes       `));
-        }
-        const percentages = calculatePercentages(pollInfo.voteCounts);
-        messageArray.push(convertAnswerLine('A', pollInfo.poll.a, `${percentages.a.pct}%`, percentages.a.isWinning));
-        messageArray.push(convertAnswerLine('B', pollInfo.poll.b, `${percentages.b.pct}%`, percentages.b.isWinning));
-        if (pollInfo.poll.c !== '') {
-          messageArray.push(convertAnswerLine('C', pollInfo.poll.c, `${percentages.c.pct}%`, percentages.c.isWinning));
-        } else {
-          messageArray.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-        }
-        sendMessage({
+    try {
+      const pollInfo = await poll.getBySub(data.subId);
+      let pollClosed = false;
+      if (
+        !pollInfo.poll.isOpen ||
+        new Date() > moment(pollInfo.poll.openUntil).toDate()
+      ) {
+        console.log('Poll is closed or expired');
+        pollClosed = true;
+      }
+      const messageArray = [
+        // Title line
+        [
+          65, 65, 65, 65, 65, 65, 65, 0, 0, 16, 15, 12, 12, 0, 0, 65, 65, 65,
+          65, 65, 65, 65,
+        ],
+      ];
+      messageArray.push(convertLine(`  vbpoll.app/${pollInfo.pollID}  `));
+      if (pollInfo.voteCounts.totalVotes === 1 && !pollClosed) {
+        messageArray.push(convertLine('        1 vote        '));
+      } else if (pollClosed) {
+        console.log('Poll is closed');
+        messageArray.push(
+          convertLine(
+            `  closed - ${pollInfo.voteCounts.totalVotes} vote${
+              pollInfo.voteCounts.totalVotes > 1 ? 's' : ''
+            }  `,
+          ),
+        );
+      } else {
+        messageArray.push(
+          convertLine(`       ${pollInfo.voteCounts.totalVotes} votes       `),
+        );
+      }
+      const percentages = calculatePercentages(pollInfo.voteCounts);
+      messageArray.push(
+        convertAnswerLine(
+          'A',
+          pollInfo.poll.a,
+          `${percentages.a.pct}%`,
+          percentages.a.isWinning,
+        ),
+      );
+      messageArray.push(
+        convertAnswerLine(
+          'B',
+          pollInfo.poll.b,
+          `${percentages.b.pct}%`,
+          percentages.b.isWinning,
+        ),
+      );
+      if (pollInfo.poll.c !== '') {
+        messageArray.push(
+          convertAnswerLine(
+            'C',
+            pollInfo.poll.c,
+            `${percentages.c.pct}%`,
+            percentages.c.isWinning,
+          ),
+        );
+      } else {
+        messageArray.push([
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ]);
+      }
+      sendMessage(
+        {
           subId: data.subId,
           apiKey: data.apiKey,
           apiSecret: data.apiSecret,
-        }, messageArray);
-      },
-  } catch(error) {
-    console.log(error)
-  }
+        },
+        messageArray,
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
